7a243917faf23193eb58e87efbe22aef82ed8b83
Eneuem <118232833+Eneuem@users.noreply.github.com>
2023-11-22 23:06:52 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,15 +2,33 @@
 function CreateWebServer() {
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World!\n');
-});
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/comment');
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
+// controllers/comment.js
+
+// Liste des commentaires
+exports.comment_list = (req, res) => {
+    // Logique pour récupérer et renvoyer la liste des commentaires
+    res.send('Liste des commentaires');
+};
+
+// Ajouter un commentaire
+exports.comment_add = (req, res) => {
+    // Logique pour ajouter un commentaire
+    res.send('Ajouter un commentaire');
+};
+
+// Autres méthodes (supprimer, modifier, etc.) peuvent être ajoutées ici
+// Suite de votre fichier de routeur
+
+// Route pour obtenir la liste des commentaires
+router.get('/', commentController.comment_list);
+
+// Route pour ajouter un commentaire
+router.post('/add', commentController.comment_add);
+
+// Vous pouvez ajouter d'autres routes pour des actions comme modifier ou supprimer
 }
-
-CreateWebServer();
 
