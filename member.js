a26722c5b0bb0426f9c0f66809209a26f62cb246
Eneuem <118232833+Eneuem@users.noreply.github.com>
2023-11-22 22:45:26 +0000

    Copilot third commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/skills/members.html',
+      controller: 'SkillsMembersCtrl',
+      controllerAs: 'skillsMembersCtrl',
+      bindToController: true,
+      scope: {
+        members: '='
+      }
+    };
+  }
